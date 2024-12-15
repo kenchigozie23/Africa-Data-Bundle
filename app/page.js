c00ee614f951f31e1d510ba209106ea2d38094d9
Ken Chigozie <kenronkwo@gmail.com>
2024-12-15 08:56:41 +0100

    data

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Firstnav from "@/components/navbar";
+
+export default function Home() {
+  return (
+ <div className='bg-[#] '>
+    <Firstnav/>
+ 
+ </div>
+  );
+
+}
